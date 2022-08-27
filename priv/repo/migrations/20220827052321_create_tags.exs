baccc01b196565f9bc23eafa2fb8c60c543e1335
Herman verschooten <Herman@verschooten.net>
2022-08-27 08:14:37 +0200

    Remove remnnants of Ecto

--- priv/repo/migrations/20220827052321_create_tags.exs
+++
@@ -1,9 +0,0 @@
-defmodule Til.Repo.Migrations.CreateTags do
-  use Ecto.Migration
-
-  def change do
-    create table(:tags) do
-      add :name, :string
-    end
-  end
-end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,1 @@
 ExUnit.start()
-Ecto.Adapters.SQL.Sandbox.mode(Til.Repo, :manual)
